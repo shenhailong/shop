76f7193f4021993d17d137103e1f978051896046
shenhailong <819062373@qq.com>
2020-07-23 07:46:13 +0800

    添加样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+  <div class="home aaa">
+    <img alt="Vue logo" src="../assets/logo.png" class="aaa" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
