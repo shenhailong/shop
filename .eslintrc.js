7550458d27bc0c566c2172e2ef561ec26de24f95
shenhailong <819062373@qq.com>
2020-07-24 17:15:50 +0800

    替换swiper版本

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,13 @@ module.exports = {
   rules: {
     "quotes": [2, "single"],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "space-infix-ops": ["error", {"int32Hint": false}],
+    "indent": ["error", 2],
+    "key-spacing": ["error", { "beforeColon": false }],
+    "lines-between-class-members": ["error", "always"],
+    "no-redeclare": "error",
+    "comma-spacing": ["error", { "before": false, "after": true }],
+    "no-unneeded-ternary": "error"
   }
 };
--- src/views/index/Index.vue
+++ src/views/index/Index.vue
@@ -1,15 +1,25 @@
 <template>
   <div class="wrap-index">
     <NavBar current="index" />
+
     <div class="content">
-      <swiper ref="mySwiper" class="swiper-wrap" :options="swiperOptions">
-        <swiper-slide v-for="(item, index) in swiperList" :key="index" class="slider">
-          <a :href="item.url">
+      <!-- Swiper -->
+      <div class="swiper-container">
+        <div class="swiper-wrapper ">
+          <div class="swiper-slide slider" v-for="(item, index) in swiperList" :key="index">
+            <a :href="item.url">
             <img :src="item.src" />
-          </a>
-        </swiper-slide>
-        <div class="swiper-pagination" slot="pagination"></div>
-      </swiper>
+            </a>
+          </div>
+        </div>
+        <!-- Add Pagination -->
+        <div class="swiper-pagination swiper-pagination-white" id="swiper-spit"></div>
+        <!-- Add Arrows -->
+        <div class="swiper-button-next swiper-button-white"></div>
+        <div class="swiper-button-prev swiper-button-white"></div>
+      </div>
+
+      <!-- 产品列表 -->
       <div class="product-list">
         <Product />
       </div>
@@ -18,20 +28,17 @@
 </template>
 
 <script>
-import { Swiper, SwiperSlide, directive } from 'vue-awesome-swiper'
-import 'swiper/swiper-bundle.min.css'
+import Swiper from 'swiper';
+import 'swiper/dist/css/swiper.min.css'
+import 'swiper/dist/js/swiper.min.js'
 import NavBar from '@components/NavBar'
 import Product from '@components/Product'
 export default {
   components: {
-    Swiper,
-    SwiperSlide,
     NavBar,
     Product
   },
-  directives: {
-    swiper: directive
-  },
+
   data() {
     return {
       swiperList: [
@@ -47,25 +54,36 @@ export default {
           url: 'https://cn.vuejs.org/v2/guide/',
           src: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=668925903,3991018893&fm=26&gp=0.jpg'
         }
-      ],
-      swiperOptions: {
-        pagination: {
-          el: '.swiper-pagination'
-        },
-        // Some Swiper option/callback...
-      }
-    }
-  },
-  computed: {
-    swiper() {
-      return this.$refs.mySwiper.$swiper
+      ]
     }
   },
   mounted() {
-    this.swiper.slideTo(3, 1000, false)
+    this.initSwiper()
   },
   methods: {
+    initSwiper () {
+      var swiper = new Swiper('.swiper-container', {
+        spaceBetween: 30,
+        effect: 'fade',
+        fadeEffect: {
+          crossFade: true,
+        },
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false, // 取消鼠标操作后的轮播暂停【无操作轮播继续】【参考链接1】
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
 
+      });
+      console.log(swiper)
+    }
   }
 }
 </script>
@@ -75,12 +93,17 @@ export default {
 .content{
   padding-top: 105px;
 }
-.swiper-wrap{
+.swiper-container{
   width: 1170px;
   height: 300px;
+  margin: 0 auto;
   .slider{
     width: 100%;
     height: 100%;
+  }
+  img{
+    height: 100%;
+    width: 100%;
   }
 }
 </style>
