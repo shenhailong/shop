499f34659eeac0bcad7a56dcb519b922b09e8ad0
shenhailong <819062373@qq.com>
2020-07-30 08:00:08 +0800

    Empty

---
+++ src/components/Empty.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="empty">
+    <div class="empty-icon"></div>
+    <div class="tip" v-if="type == 'product'">
+      没有您要搜索的商品，试试其它条件吧～～～
+    </div>
+    <div class="tip" v-if="type == 'cart'">
+      您的购物车还没有添加商品，去<span class="product-link">产品中心</span>逛逛吧～～～
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: ''
+    }
+  },
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.empty{
+  padding: 100px 0 0 0px;
+  width: 500px;
+  margin: 0 auto;
+  text-align: center;
+  .empty-icon{
+    height: 80px;
+    width: 80px;
+    margin: 0 auto;
+    background: url(~@/assets/empty.png) no-repeat;
+    background-size: cover;
+  }
+
+  .tip{
+    font-size: 16px;
+    margin-top: 40px;
+    color: #999999;
+  }
+
+  .product-link{
+    color: #3686DC;
+    margin: 0 5px;
+    cursor: pointer;
+
+    &:hover{
+      font-size: 18px;
+    }
+  }
+}
+</style>
--- src/views/cart/Index.vue
+++ src/views/cart/Index.vue
@@ -1,26 +1,110 @@
 <template>
   <div class="wrap-index">
     <NavBar current="cart" />
+    <div class="content">
+      <Empty v-if="list.length === 0" type="cart" />
+      <div v-else>
+        <el-table
+          :data="list"
+          @select-all="selectAll"
+          @select="select"
+          style="width: 100%"
+          border
+          size="mini">
+          <el-table-column
+            type="selection"
+            align="center"
+            width="55">
+          </el-table-column>
+          <el-table-column
+            prop="name"
+            label="产品名称"
+            align="center"
+            width="150">
+          </el-table-column>
+          <el-table-column
+            prop="name"
+            label="产品明细"
+            align="center"
+            width="150">
+          </el-table-column>
+          <el-table-column prop="name" label="时间单位" align="center">
+            <template slot-scope="scope">
+              <el-radio-group v-model="scope.row.mode">
+                <el-radio label="year">年</el-radio>
+                <el-radio label="month">月</el-radio>
+                <el-radio label="date">日</el-radio>
+              </el-radio-group>
+            </template>
+          </el-table-column>
+          <el-table-column align="center" label="购买时长" width="160">
+            <template slot-scope="scope" align="center" >
+              <el-input-number size="mini" v-model="scope.row.count" :min="1" :max="10"></el-input-number>
+            </template>
+          </el-table-column>
+          <el-table-column align="center" label="购买数量" width="160">
+            <template slot-scope="scope">
+              <el-input-number v-model="scope.row.num" :min="1" :max="10" size="mini"></el-input-number>
+            </template>
+          </el-table-column>
+          <el-table-column align="center" label="价格" width="120">
+            <template slot-scope="scope">
+              {{scope.row.price}}
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="操作"
+            width="100"
+            align="center">
+            <template slot-scope="scope">
+              <el-button @click="uploadVoucher(scope.row)" type="danger" size="small">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+
+        <div class="footer">dd</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import NavBar from '@components/NavBar'
+import Empty from '@components/Empty'
 
 export default {
   components: {
-    NavBar
+    NavBar,
+    Empty
   },
   data() {
     return {
-
+      list: [{
+        id: 1,
+        num: 0
+      }]
     }
   },
   mounted() {
 
   },
   methods: {
-
+    selectAll(selection) {
+      console.log(selection)
+    },
+    select(selection, row){
+      console.log(selection)
+      console.log(row)
+    },
+    delete(){}
   }
 }
 </script>
+
+
+<style lang="scss" scoped>
+.footer{
+  width: 100%;
+  background: #ffffff;
+}
+</style>
