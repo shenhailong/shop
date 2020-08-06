3231c138c7ea214e21a4e98d89be72bdf3dd8c7b
hailong.shen <hailong.shen@simceredx.com>
2020-08-06 17:37:47 +0800

    支付

--- src/views/user/PayIndex.vue
+++ src/views/user/PayIndex.vue
@@ -4,7 +4,7 @@
  * @Author: Dragon
  * @Date: 2020-07-27 17:27:10
  * @LastEditors: Dragon
- * @LastEditTime: 2020-08-06 10:26:07
+ * @LastEditTime: 2020-08-06 14:37:33
 -->
 <template>
   <div class="member-select">
@@ -33,11 +33,25 @@
         </el-radio-group>
       </div>
       <el-radio-group v-model="payType">
-        <el-radio :label="3">微信</el-radio>
-        <el-radio :label="6">中级会员</el-radio>
-        <el-radio :label="9">高级会员</el-radio>
-        <el-radio :label="10">终身会员</el-radio>
+        <el-radio :label="3">
+          <el-radio :label="3">微信支付
+            <div class="wx-logo"></div>
+          </el-radio>
+        </el-radio>
+        <el-radio :label="3">
+          <el-radio :label="3">微信支付
+            <div class="wx-logo"></div>
+          </el-radio>
+        </el-radio>
+        <el-radio :label="3">
+          <el-radio :label="3">微信支付
+            <div class="wx-logo"></div>
+          </el-radio>
+        </el-radio>
       </el-radio-group>
+      <div class="wx-logo"></div>
+      <div class="ali-logo"></div>
+      <div class="card-logo"></div>
     </div>
   </div>
 </template>
@@ -69,6 +83,28 @@ export default {
 .member-select{
   .content{
     background: #ffffff;
+  }
+  .wx-logo{
+    height: 64px;
+    width: 64px;
+    background: url(~@/assets/wx.png) no-repeat;
+    background-size: contain;
+    cursor: pointer;
+  }
+  .ali-logo{
+    height: 56px;
+    width: 56px;
+    background: url(~@/assets/ali.png) no-repeat;
+    background-size: contain;
+    cursor: pointer;
+  }
+
+  .card-logo{
+    height: 64px;
+    width: 64px;
+    background: url(~@/assets/card.png) no-repeat;
+    background-size: contain;
+    cursor: pointer;
   }
 }
 </style>
