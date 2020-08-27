3be99ee84c60c5e1630abd87aa6a3f51a6fc05c8
hailong.shen <hailong.shen@simceredx.com>
2020-08-27 18:30:36 +0800

    订单

--- src/components/order/Origin.vue
+++ src/components/order/Origin.vue
@@ -51,7 +51,7 @@
         width="150"
         align="center">
         <template slot-scope="scope">
-          <span @click="goDetail(scope.row.orderno)" class="link">{{ scope.row.orderno }}</span>
+          <el-link @click="goDetail(scope.row.orderno)" type="primary">{{ scope.row.orderno }}</el-link>
         </template>
       </el-table-column>
       <el-table-column
@@ -98,6 +98,14 @@
         align="center">
       </el-table-column>
     </el-table>
+    <div class="pagination">
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        @current-change="currentChange"
+        :total="total">
+      </el-pagination>
+    </div>
   </div>
 </template>
 <script>
@@ -109,7 +117,9 @@ export default {
         name: '',
         id: '',
         start: '',
-        end: ''
+        end: '',
+        curPage: 1, // 当前页
+        pageSize: 12,
       },
       list: [{
         orderno: 'O-00108823',
@@ -121,7 +131,11 @@ export default {
         ccorpname: '中国电子科技网络信息安全有限公司',
         tel: '010-58278923'
       }],
-      date: []
+      date: [],
+      curPage: 1, // 当前页
+      pageSize: 12,
+      total: 0,
+      loading: false
     }
   },
   mounted() {
@@ -130,7 +144,7 @@ export default {
   methods: {
     getList() {
       this.loading = true
-      this.$axios.post('originorder.list', this.ruleForm).then((res) => {
+      this.$axios.post('originorder.check', this.ruleForm).then((res) => {
         if (res.code === 0) {
           this.list = res.data.list
         }
@@ -140,6 +154,11 @@ export default {
     },
     // 查询
     search() {
+      this.getList()
+    },
+    // 页数切换
+    currentChange(value) {
+      this.curPage = value
       this.getList()
     },
     resetForm(formName) {
@@ -167,5 +186,10 @@ export default {
 
 .reset{
   margin-left: 30px;
+}
+
+.pagination{
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
--- src/views/order/Detail.vue
+++ src/views/order/Detail.vue
@@ -4,13 +4,59 @@
  * @Author: Dragon
  * @Date: 2020-08-27 16:08:54
  * @LastEditors: Dragon
- * @LastEditTime: 2020-08-27 16:11:59
+ * @LastEditTime: 2020-08-27 18:25:52
 -->
 <template>
   <div class="wrap-index">
     <NavBar current="order" />
     <div class="content">
-      
+      <el-row class="row-line">
+        <el-col :span="8">
+          <span class="label">订单号码: </span>{{detail.orderno}}
+        </el-col>
+        <el-col :span="8">
+          <span class="label">客户订阅号: </span>{{detail.custid}}
+        </el-col>
+        <el-col :span="8">
+          <span class="label">产品编号: </span>{{detail.prodno}}
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="8">
+          <span class="label">产品描述: </span>{{detail.proddesc}}
+        </el-col>
+        <el-col :span="8">
+          <span class="label">产品数量: </span>{{detail.amount}}
+        </el-col>
+        <el-col :span="8">
+          <span class="label">订单时间: </span>{{detail.orderdt}}
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="8">
+          <span class="label">订单号码: </span>{{detail.orderno}}
+        </el-col>
+        <el-col :span="8">
+          <span class="label">客户订阅号: </span>{{detail.custid}}
+        </el-col>
+        <el-col :span="8">
+          <span class="label">产品编号: </span>{{detail.prodno}}
+        </el-col>
+      </el-row>
+
+      <el-row>
+        <el-col :span="8">
+          <span class="label">订单号码: </span>{{detail.orderno}}
+        </el-col>
+        <el-col :span="8">
+          <span class="label">客户订阅号: </span>{{detail.custid}}
+        </el-col>
+        <el-col :span="8">
+          <span class="label">产品编号: </span>{{detail.prodno}}
+        </el-col>
+      </el-row>
     </div>
   </div>
 </template>
@@ -24,7 +70,21 @@ export default {
   },
   data() {
     return {
-
+      detail: {
+        orderno: 'O-00108823',
+        custid: 'A-S100108823',
+        prodno: 'T9584AAE',
+        proddesc: 'LoadRunner Professional Foundation Entitlement Software E-LTU',
+        amount: '100',
+        orderdt: '2020/7/9',
+        enddt: '',
+        ccorpname: '',
+        ecorpname: '',
+        contact: '',
+        tel: '',
+        email: '',
+        shtyxydm: ''
+      }
     }
   },
   mounted() {
@@ -45,3 +105,21 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.content{
+  background: #fff;
+  color: #606266;
+  font-size: 16px;
+  padding: 150px 30px;
+  box-sizing: border-box;
+  .label{
+    font-size: 14px;
+    color: #606266;
+  }
+
+  .row-line{
+    margin-bottom: 20px;
+  }
+}
+</style>
