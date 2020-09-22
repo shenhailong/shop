24005f63c7664b0068b90f0d4a26aacf278c3ec5
hailong.shen <hailong.shen@simceredx.com>
2020-09-22 14:34:14 +0800

    调整按需加载,优化速度

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,26 @@
+/*
+ * @Description: 
+ * @Version: 1.0.0
+ * @Author: Dragon
+ * @Date: 2020-09-22 13:15:10
+ * @LastEditors: Dragon
+ * @LastEditTime: 2020-09-22 14:06:01
+ */
 module.exports = {
   presets: [
     [
       '@vue/app',
       {
         useBuiltIns: 'entry'
+      }
+    ]
+  ],
+  'plugins': [
+    [
+      'component',
+      {
+        'libraryName': 'element-ui',
+        'styleLibraryName': 'theme-chalk'
       }
     ]
   ]
--- src/main.js
+++ src/main.js
@@ -4,23 +4,105 @@
  * @Author: Dragon
  * @Date: 2020-09-22 13:15:10
  * @LastEditors: Dragon
- * @LastEditTime: 2020-09-22 13:17:09
+ * @LastEditTime: 2020-09-22 14:26:12
  */
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import axios from './utils/axios';
 import './scss/styles.scss';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import {
+  Pagination,
+  Dialog,
+  Input,
+  InputNumber,
+  Radio,
+  RadioGroup,
+  RadioButton,
+  Checkbox,
+  CheckboxButton,
+  CheckboxGroup,
+  Switch,
+  Option,
+  OptionGroup,
+  Button,
+  ButtonGroup,
+  Table,
+  TableColumn,
+  DatePicker,
+  Form,
+  FormItem,
+  Tabs,
+  TabPane,
+  Icon,
+  Row,
+  Col,
+  Upload,
+  Progress,
+  Spinner,
+  Card,
+  Steps,
+  Step,
+  Carousel,
+  CarouselItem,
+  Link,
+  Loading,
+  MessageBox,
+  Message,
+  Notification
+} from 'element-ui';
+
+Vue.use(Pagination);
+Vue.use(Dialog);
+Vue.use(Input);
+Vue.use(InputNumber);
+Vue.use(Radio);
+Vue.use(RadioGroup);
+Vue.use(RadioButton);
+Vue.use(Checkbox);
+Vue.use(CheckboxButton);
+Vue.use(CheckboxGroup);
+Vue.use(Switch);
+Vue.use(Option);
+Vue.use(OptionGroup);
+Vue.use(Button);
+Vue.use(ButtonGroup);
+Vue.use(Table);
+Vue.use(TableColumn);
+Vue.use(DatePicker);
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.use(Tabs);
+Vue.use(TabPane);
+Vue.use(Icon);
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Upload);
+Vue.use(Progress);
+Vue.use(Spinner);
+Vue.use(Card);
+Vue.use(Steps);
+Vue.use(Step);
+Vue.use(Carousel);
+Vue.use(CarouselItem);
+Vue.use(Link);
+Vue.use(Loading.directive);
+
+Vue.prototype.$loading = Loading.service;
+Vue.prototype.$msgbox = MessageBox;
+Vue.prototype.$confirm = MessageBox.confirm;
+Vue.prototype.$prompt = MessageBox.prompt;
+Vue.prototype.$notify = Notification;
+Vue.prototype.$message = Message;
 const install = function (Vue) {
   Object.defineProperty(Vue.prototype, '$axios', { value: axios })
 }
 Vue.use(install)
-Vue.use(ElementUI);
+
 Vue.config.productionTip = false;
 
 new Vue({
