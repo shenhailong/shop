44fc1b5dcf7b816423b52cc233d71fbcf5ccaaed
hailong.shen <hailong.shen@simceredx.com>
2020-10-30 16:36:05 +0800

    router

--- src/router/index.js
+++ src/router/index.js
@@ -175,4 +175,11 @@ router.beforeEach((to, from, next) => {
   }
 })
 
+// 解决Vue-Router升级导致的Uncaught(in promise) navigation guard问题
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push (location, onResolve, onReject) {
+  if (onResolve || onReject) return originalPush.call(this, location, onResolve, onReject)
+  return originalPush.call(this, location).catch(err => err)
+}
+
 export default router;
